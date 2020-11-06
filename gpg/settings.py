c20566db40f9cc2bd46971be55de4c31275c9e99
Garrett <gkatashima@gmail.com>
2020-11-06 15:32:28 -0800

    created listing schema

--- gpg/settings.py
+++ gpg/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'gpg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gpgdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Dhoward23!',
+        'HOST': 'localhost'
     }
 }
 
