b63a1fd5277e9328a4979b02ea14f25223aa2a23
Garrett <gkatashima@gmail.com>
2020-11-11 12:58:13 -0800

    added features to django website

--- gpg/settings.py
+++ gpg/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '@i%l3&w!04iu17_3xi^gw^z26d2mukwl^a+a6!m0tjeopb3qx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,3 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'gpg/static')
 ]
+
+#Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
