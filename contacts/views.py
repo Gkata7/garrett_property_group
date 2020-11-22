ccf04c054ad890ac63d3eb383ea80f4609c37a1e
Garrett <gkatashima@gmail.com>
2020-11-21 16:03:18 -0800

    added more styling to each template page

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+# from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -12,8 +13,25 @@ def contact(request):
     message = request.POST['message']
     user_id = request.POST['user_id']
     realtor_email = request.POST['realtor_email']
+# Check to see if user made inquiry already
+    if request.user.is_authenticated:
+      user_id = request.user.id 
+      has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+      if has_contacted:
+        messages.error(request, 'You have already made an inquiry')
+        return redirect('/listings/'+listing_id)
+    
     contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
 
     contact.save()
+# Send Email
+    # send_mail (
+    #   'Property Listing Inquiry',
+    #   'There has been an inquiry for ' + listing + '. Sign in the admin panel for more info',
+    #   'email here',
+    #   [realtor_email, 'email here'],
+    #   fail_silently=False
+    # )
+
     messages.success(request, 'Your request has been submitted, a realtor will reach out shortly.')
     return redirect('/listings/'+listing_id)
