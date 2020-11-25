5564a76d12375a8c39cdb8d0fc161ae371c2092e
Garrett <gkatashima@gmail.com>
2020-11-25 14:10:59 -0800

    make it better

--- gpg/settings.py
+++ gpg/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,7 +135,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'gpg/static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
