ce8a636328127442bb29e0c916c94a71860debeb
Garrett Katashima <garrettkatashima@Garretts-MacBook-Pro.local>
2022-02-02 01:18:31 -0800

    update settings

--- gpg/settings.py
+++ gpg/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@i%l3&w!04iu17_3xi^gw^z26d2mukwl^a+a6!m0tjeopb3qx6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
